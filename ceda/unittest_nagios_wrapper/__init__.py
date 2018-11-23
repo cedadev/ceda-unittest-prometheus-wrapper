606781afa49f1c9ccfa5ba602767e81d4500dc41
Philip Kershaw <philip.kershaw@stfc.ac.uk>
2018-11-23 17:02:44 +0000

    Updated header

---
+++ ceda/unittest_nagios_wrapper/__init__.py
@@ -0,0 +1,7 @@
+"""Package to wrap unittest test case as a Nagios script
+"""
+__author__ = "P J Kershaw"
+__date__ = "21/11/17"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
+__license__ = """BSD - See LICENSE file in top-level directory"""
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
